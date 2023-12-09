21fa389a9159bc0116cf219225524d26eb8cf176
Flight3R <michaal747@gmail.com>
2023-12-09 19:45:38 +0100

    Added about product page
    Modified about page

--- frontend/src/app/about-page/about-page.component.ts
+++ frontend/src/app/about-page/about-page.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-about',
+  selector: 'app-about-page',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.css'
 })
-export class AboutComponent {
+export class AboutPageComponent {
 
 }
